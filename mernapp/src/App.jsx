9e375e6b76181f39b7e1cade34e8b02720df4306
SIVASURYA <112889318+sivasurya01@users.noreply.github.com>
2024-08-04 23:13:58 -0700

    first commit

--- mernapp/src/App.jsx
+++ mernapp/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./Users";
 import Updateuser from "./Updateuser";
@@ -7,11 +7,35 @@ import CreateTodo from "./CreateTodo";
 import Register from "./Register";
 import Login from "./Login";
 function App() {
+  const Lazyuser = lazy(() => import("./Users"));
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Users />}></Route>
+          {/* <Suspense fallback={<p>Loading...</p>}> */}
+          <Route
+            path="/"
+            element={
+              <Suspense
+                fallback={
+                  <p
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      justifyItems: "center",
+                      marginTop: "30px",
+                    }}
+                  >
+                    Loading...
+                  </p>
+                }
+              >
+                <Lazyuser />
+              </Suspense>
+            }
+          ></Route>
+          {/* </Suspense> */}
           <Route path="/Register" element={<Register />}></Route>
           <Route path="/Login" element={<Login />}></Route>
           <Route path="/CreateUser" element={<CreateUser />}></Route>
