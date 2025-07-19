f0acbe4494f087a47dc07e059b069360b869eaf5
SIVASURYA <112889318+sivasurya01@users.noreply.github.com>
2025-07-19 12:34:55 -0800

    auth changes

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ async function connectDB() {
 
 connectDB();
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ message: "Unauth" });
   }
